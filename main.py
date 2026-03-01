bf2f832db4be1612d97092847c66af375a7715b2
aditya <adityam14671@gmail.com>
2026-03-01 09:10:25 +0530

    fixed signup function and bcrypt issue

--- main.py
+++ main.py
@@ -18,9 +18,12 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 60
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str):
+    # bcrypt supports max 72 bytes
+    password = password[:72]
     return pwd_context.hash(password)
 
-def verify_password(plain_password, hashed_password):
+def verify_password(plain_password: str, hashed_password: str):
+    plain_password = plain_password[:72]
     return pwd_context.verify(plain_password, hashed_password)
 
 # ===== FAKE DATABASE =====
